46eb8d61ae7ebd786c4bae6c26d98a60a18b0ac1
Guinea <guinea@pig.com>
2019-12-31 10:59:23 +0000

    Add x and y velocity

--- tui-game.py
+++ tui-game.py
@@ -1,26 +1,49 @@
 #!/usr/bin/env python3
+
 import curses
-import curses.ascii
  
 def mainloop(scr):
-    y = 2
-    x = 2
-    curses.curs_set(0)
-    scr.keypad(True)
+    xvel = 0
+    yvel = 0
+    y = 10
+    x = 10
 
-    scr.addstr(y,x, "o")
+    # Hide the cursor
+    curses.curs_set(0)
+
+    scr.addstr(y,x, "Z")
     while True:
+
+        # Wait one second for user input
+        # If no key pressed, continue anyway (after one second)
         curses.halfdelay(10)
+
+        # ch gets value of pressed key
         ch = scr.getch()
-        if ch == -1:
-            x += 1
-        elif ch == 113:     # q=quit
+
+#        if ch == -1:        # no key was pressed
+#            x = x + 1
+        if ch == 113:       # q=quit
             break
-        else:
-            y += 1
+        elif ch == 258:
+            yvel = 1
+        elif ch == 261:
+            xvel = 1
+            yvel = 0
+        elif ch == 32:
+            xvel = 0
+            yvel = 0
+
+        x = x + xvel
+        y = y + yvel
 
         scr.erase()
-        scr.addstr(y,x, "o")
+
+        # print something at position x, y
+        scr.addstr(y,x, "Z")
+
+        scr.addstr(0,0,"ch={} x={} y={} xvel={} yvel={}".format(ch, x, y, xvel, yvel))
         scr.refresh()
      
 curses.wrapper(mainloop)
+
