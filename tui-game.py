0fd3b855252c52ff4014efb4dcdba8185dd592ee
Guinea <guinea@pig.com>
2019-12-31 11:02:30 +0000

    Make all arrow keys work

--- tui-game.py
+++ tui-game.py
@@ -10,7 +10,7 @@ def mainloop(scr):
     yvel = 0
     y = 10
     x = 10
-    speed = 10
+    brake = 10
     # Hide the cursor
     curses.curs_set(0)
 
@@ -19,7 +19,7 @@ def mainloop(scr):
 
         # Wait one second for user input
         # If no key pressed, continue anyway (after one second)
-        curses.halfdelay(speed)
+        curses.halfdelay(brake)
 
         # ch gets value of pressed key
         ch = scr.getch()
@@ -30,16 +30,23 @@ def mainloop(scr):
             break
         elif ch == 258:
             yvel = 1
+            xvel = 0
         elif ch == 261:
             xvel = 1
+            yvel = 0
+        elif ch == 259:
+            yvel = -1
+            xvel = 0
+        elif ch == 260:
+            xvel = -1
             yvel = 0
         elif ch == 32:
             xvel = 0
             yvel = 0
         elif ch == 119:
-            speed = speed + 1
+            brake = brake + 1
         elif ch == 115:
-            speed = speed - 1
+            brake = brake - 1
         x = x + xvel
         y = y + yvel
 
@@ -51,7 +58,7 @@ def mainloop(scr):
         # print something at position x, y
         scr.addstr(y,x, "Z")
 
-        scr.addstr(0,0,"ch={} x={} y={} xvel={} yvel={} speed={}".format(ch, x, y, xvel, yvel,speed))
+        scr.addstr(0,0,"ch={} x={} y={} xvel={} yvel={} brake={}".format(ch, x, y, xvel, yvel,brake))
         scr.refresh()
      
 curses.wrapper(mainloop)
